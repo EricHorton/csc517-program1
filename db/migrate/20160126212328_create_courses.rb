5dbf5e8462081cc50d543c577de130b54cdfb5be
saiteja09 <saiteja09@gmail.com>
2016-01-26 17:00:13 -0500

    adding courses db

---
+++ db/migrate/20160126212328_create_courses.rb
@@ -0,0 +1,16 @@
+class CreateCourses < ActiveRecord::Migration
+  def change
+
+    create_table :courses do |t|
+      t.string :coursenumber,  null:false
+      t.string :title, null:false
+      t.string :description, null:false
+      t.date :start_date, null:false
+      t.date :end_date, null:false
+      t.timestamps null: false
+      t.references :instructor, index: true
+    end
+
+    add_foreign_key :courses, :users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126144554) do
+ActiveRecord::Schema.define(version: 20160126212328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "coursenumber", null: false
+    t.string   "title",        null: false
+    t.string   "description",  null: false
+    t.date     "start_date",   null: false
+    t.date     "end_date",     null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "user_id"
+  end
+
+  add_index "courses", ["user_id"], name: "index_courses_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                           null: false
@@ -28,4 +41,5 @@ ActiveRecord::Schema.define(version: 20160126144554) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
+  add_foreign_key "courses", "users"
 end
