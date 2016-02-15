97a5df2c3e591f8dcf5a5a505562ed0d000f3aec
EricHorton <ewhorton@ncsu.edu>
2016-02-15 17:33:27 -0500

    Adding todo. Fixing user edit password field.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  # TODO should this be exception instead?
   protect_from_forgery with: :null_session
 
   # Force login
