9fcd44c3978b19249ec0ada0d1700631c6dde705
EricHorton <ewhorton@ncsu.edu>
2016-02-16 09:04:51 -0500

    Fixing tests.

--- test/controllers/admins_controller_test.rb
+++ test/controllers/admins_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class AdminsControllerTest < ActionController::TestCase
   test "should get index" do
+    session[:user_id] = Admin.first.id
     get :index
     assert_response :success
   end
